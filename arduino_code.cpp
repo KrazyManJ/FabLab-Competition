0175c0bceca32f80bd2f68461b3dc59689e44f78
KrazyManJ <KrazyManJ@gmail.com>
2022-03-30 19:00:48 +0200

    Added time/date switch, moved values as #define constants, re-made whole circuit design

--- arduino_code.cpp
+++ arduino_code.cpp
@@ -1,27 +1,33 @@
 #include <LiquidCrystal.h>
 
 //
-// DEFINING
+// DEFINING CONSTANTS
 //
 
 #define LCD_SIZE 16
-#define SCROLL_TEXT_SPEED 100
+#define SCROLL_TEXT_SPEED 50
+#define TIME_DATE_SWITCH_DELAY 5000
+//PINS
+#define BUTTON_READ 2
+#define BUTTON_OUTPUT 3
+#define TEMP_INPUT_PIN A1
+#define CONTRAST_OUTPUT A0
 
 //
 // LCD UTILS
 //
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 
 byte degree[] = {
-  B11100,
-  B10100,
-  B11100,
+  B01110,
+  B01010,
+  B01110,
   B00000,
-  B00011,
-  B00100,
-  B00100,
-  B00011
+  B00000,
+  B00000,
+  B00000,
+  B00000
 };
 
 //
@@ -29,13 +35,15 @@ byte degree[] = {
 //
 
 void setup(){
-  pinMode(7,OUTPUT);
-  pinMode(8,INPUT);
-  analogWrite(6,0);
-  analogWrite(A1,255);
+  pinMode(TEMP_INPUT_PIN,INPUT);
+  pinMode(CONTRAST_OUTPUT,OUTPUT);
+  pinMode(BUTTON_OUTPUT, OUTPUT);
+  pinMode(BUTTON_READ, INPUT);
+  
+  analogWrite(CONTRAST_OUTPUT,0);
   lcd.createChar(0, degree);
   lcd.begin(16, 2);
-  Serial.begin(9600);
+//  Serial.begin(9600);
 }
 
 //
@@ -43,9 +51,12 @@ void setup(){
 //
 
 void loop(){
+  bool pressed = false;
+  bool switched = false;
+  
   writeFadedText(0,"www.vassboskovice.cz");
   writeTimeDate(0,1);
-  writeTemp(13,1);
+  writeTemp(12,1);
   handleButton();
 }
 
@@ -57,18 +68,13 @@ bool pressed = false;
 bool switched = false;
 
 void handleButton(){
-  int btn = digitalRead(8);
-  Serial.println(btn);
+  int btn = digitalRead(BUTTON_READ);
   if (btn == 1 && !pressed){
     pressed = true;
-    switchLED();
+    switched = !switched;
+  	digitalWrite(BUTTON_OUTPUT,switched);
   }
   else if (btn == 0 && pressed) pressed = false;
-}
-
-void switchLED(){
-  switched = !switched;
-  digitalWrite(7,switched);
 }
 
 //
@@ -82,26 +88,34 @@ void writeTemp(int x,int y){
   lcd.setCursor(x,y);
   lcd.print((String(temp).length() == 1 ? " " : "")+String(temp));
   lcd.write(byte(0));
+  lcd.print("C");
 }
 
 int trackTemp(){
-  return (int) map(analogRead(A0),20,358,-40,125);
+  return (int) map(analogRead(TEMP_INPUT_PIN),20,358,-40,125);
 }
 
 //
 // DATE
 //
 
+long dt_switch = 0;
+bool date_or_time = false;
+
 void writeTimeDate(int x, int y){
+  if (millis() > dt_switch+TIME_DATE_SWITCH_DELAY){
+    dt_switch = millis();
+    date_or_time = !date_or_time;
+  }
   lcd.setCursor(x,y);
-  lcd.print("30.03.12:05");
+  lcd.print(date_or_time ? "30.03." : "19:00 ");
 }
 
 //
 // FADED TEXT
 //
 
-float lastChanged = 0;
+long lastChanged = 0;
 int pr = 0;
 
 void writeFadedText(int y,String text){
