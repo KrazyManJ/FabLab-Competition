e68af262dcddaddb503182e8db7ec8bff2f0bd8f
KrazyManJ <KrazyManJ@gmail.com>
2022-05-03 20:41:42 +0200

    Added Documentation to code, added constants, renamed methods

--- src/arduino_code.cpp
+++ src/arduino_code.cpp
@@ -7,18 +7,16 @@
 #include <Adafruit_Sensor.h>
 #include <virtuabotixRTC.h>
 
-//
-// CONSTANTS
-//
-
 #define LCD_ADRESS 0x27
-#define LCD_SIZE 16
+#define LCD_WIDTH 16
+#define LCD_HEIGHT 2
 #define LCD_DEFAULT_TEXT "www.vosassboskovice.cz"
 #define SCROLL_TEXT_SPEED 800
 #define TIME_DATE_SWITCH_DELAY 15000
 #define COLON_BLINK_TIME 600
 
 #define TEMP_INPUT_PIN 2
+#define TEMP_MODULE_TYPE DHT11
 
 #define SD_CS_PIN 53
 #define SPEAKER_PIN 9
@@ -27,10 +25,13 @@
 #define RTC_DAT_PIN 6
 #define RTC_RST_PIN 7
 
-//
-// MILLIS TIMER
-//
+//=====================================================================================
+//											MILLIS TIMER
+//=====================================================================================
 
+/**
+ * Class for handling timer using millis() function
+ **/
 class MillisTimer
 {
 	unsigned long starttime;
@@ -53,44 +54,34 @@ public:
 	}
 };
 
-//
-// LCD UTILS
-//
+//=====================================================================================
+//											LCD
+//=====================================================================================
 
-LiquidCrystal_I2C lcd(LCD_ADRESS, LCD_SIZE, 2);
+LiquidCrystal_I2C lcd(LCD_ADRESS, LCD_WIDTH, LCD_HEIGHT);
 
-byte warning[8] = {
-	B00100,
-	B00100,
-	B01110,
-	B01010,
-	B01010,
-	B11111,
-	B11011,
-	B11111};
-byte temp[8] = {
-	B00100,
-	B01010,
-	B01010,
-	B01010,
-	B01010,
-	B10001,
-	B10001,
-	B01110,
-};
+byte warningChar[8] = {0x04, 0x04, 0x0E, 0x0A, 0x0A, 0x1F, 0x1B, 0x1F};
+byte tempChar[8] = {0x04, 0x0A, 0x0A, 0x0A, 0x0A, 0x11, 0x11, 0x0E};
 
+
+/**
+ * Initialize LCD display and register custom characters
+ **/
 void initializeLCD()
 {
 	lcd.init();
 	lcd.backlight();
-	lcd.createChar(0, warning);
-	lcd.createChar(1, temp);
+	lcd.createChar(0, warningChar);
+	lcd.createChar(1, tempChar);
 }
 
-//
-// UTILS METHODS
-//
+//=====================================================================================
+//										UTILS METHODS
+//=====================================================================================
 
+/**
+ * Repeat sequence of string 
+ **/
 String repeat(String s, int times)
 {
 	int c = times;
@@ -100,17 +91,27 @@ String repeat(String s, int times)
 	return r;
 }
 
+/**
+ * Format non-decimal as string with specific length
+ * 
+ * Example:
+ * - numberStr(18,4) => 0018
+ * - numberStr(3,2) => 03
+ **/
 String numberStr(int i, int length)
 {
 	return repeat("0", length - String(i).length()) + String(i);
 }
 
-//
-// TEMP
-//
+//=====================================================================================
+//										TEMPERATURE
+//=====================================================================================
 
-DHT dht(TEMP_INPUT_PIN, DHT11);
+DHT dht(TEMP_INPUT_PIN, TEMP_MODULE_TYPE);
 
+/**
+ * Initialize temperature sensor for future usage
+ **/
 void tempInitialize()
 {
 	pinMode(TEMP_INPUT_PIN, INPUT);
@@ -120,13 +121,23 @@ void tempInitialize()
 MillisTimer warningBlink(800);
 bool warnB;
 
-int gatherTempWithBound()
+/**
+ * Gets temperature with minimum value of -9°C and maximum of 99°C
+ * 
+ * This is due to size of LCD.
+ **/
+int gatherBoundedTemp()
 {
 	int temp = dht.readTemperature();
 	temp = temp < 100 ? temp : 99;
 	return temp > -10 ? temp : -9;
 }
 
+/**
+ * Write temp at specific location.
+ * 
+ * If sensor is disconnected, then it will write temp warning text!
+ **/
 void writeTemp(int x, int y)
 {
 	if (isnan(dht.readTemperature()))
@@ -140,19 +151,22 @@ void writeTemp(int x, int y)
 		lcd.print(" ");
 		return;
 	}
-	int temp = gatherTempWithBound();
+	int temp = gatherBoundedTemp();
 	lcd.setCursor(x, y);
 	lcd.print((String(temp).length() == 1 ? " " : "") + String(temp));
 	lcd.print((char)223);
 	lcd.print("C");
 }
 
-//
-// TIME AND DATE
-//
+//=====================================================================================
+//										TIME AND DATE
+//=====================================================================================
 
 virtuabotixRTC myRTC(RTC_CLK_PIN, RTC_DAT_PIN, RTC_RST_PIN);
 
+/**
+ * Sets time to specific inputted data.
+ **/
 void updateTime(int year, int month, int day, int hour, int minutes)
 {
 	//seconds, minutes, hours, day of the week, day of the month, month, year
@@ -165,15 +179,24 @@ bool colon = false;
 MillisTimer colon_timer(COLON_BLINK_TIME);
 MillisTimer date_time(TIME_DATE_SWITCH_DELAY);
 
+/**
+ * Formats date with data from RTC
+ **/
 String formatDate()
 {
 	return numberStr(myRTC.dayofmonth, 2) + "." + numberStr(myRTC.month, 2) + "." + String(myRTC.year);
 }
+/**
+ * Formats time with data from RTC
+ **/
 String formatTime(bool colon)
 {
 	return numberStr(myRTC.hours, 2) + (colon ? ":" : " ") + numberStr(myRTC.minutes, 2);
 }
 
+/**
+ * Writes time or date specified by MillisTimer switch
+ **/
 void writeTimeDate(int x, int y)
 {
 	myRTC.updateTime();
@@ -187,14 +210,19 @@ void writeTimeDate(int x, int y)
 				  : formatTime(colon) + repeat(" ", formatDate().length() - formatTime(colon).length()));
 }
 
-//
-// FADED TEXT
-//
+//=====================================================================================
+//										SCROLLING TEXT
+//=====================================================================================
 
 String text = "";
 MillisTimer fadeTimer(SCROLL_TEXT_SPEED);
 unsigned int pr = 0;
 
+/**
+ * Reads data from SD from file "lcd.txt" and initialize text, which will scroll over.
+ * If there is no file "lcd.txt" or there is no SD card, then it will be used LCD_DEFAULT_TEXT
+ * @param hasSD if sd is loaded or not
+ **/
 void initializeFadedText(bool hasSD)
 {
 	if (hasSD)
@@ -211,6 +239,9 @@ void initializeFadedText(bool hasSD)
 	text = LCD_DEFAULT_TEXT;
 }
 
+/**
+ * Write faded text to first line of LCD
+ **/
 void writeFadedText()
 {
 	if (text == "")
@@ -225,13 +256,13 @@ void writeFadedText()
 	}
 	if (fadeTimer.isReady())
 	{
-		String out = repeat(" ", LCD_SIZE);
-		if (pr <= LCD_SIZE)
-			out = repeat(" ", LCD_SIZE - pr) + text.substring(0, pr) + repeat(" ", text.length() - pr + LCD_SIZE);
+		String out = repeat(" ", LCD_WIDTH);
+		if (pr <= LCD_WIDTH)
+			out = repeat(" ", LCD_WIDTH - pr) + text.substring(0, pr) + repeat(" ", text.length() - pr + LCD_WIDTH);
 		else if (pr <= text.length())
-			out = text.substring(pr - LCD_SIZE, pr);
-		else if (pr <= text.length() + LCD_SIZE)
-			out = text.substring(pr - LCD_SIZE) + repeat(" ", LCD_SIZE - text.substring(pr - LCD_SIZE).length());
+			out = text.substring(pr - LCD_WIDTH, pr);
+		else if (pr <= text.length() + LCD_WIDTH)
+			out = text.substring(pr - LCD_WIDTH) + repeat(" ", LCD_WIDTH - text.substring(pr - LCD_WIDTH).length());
 		else
 			pr = 0;
 		lcd.setCursor(0, 0);
@@ -240,23 +271,24 @@ void writeFadedText()
 	}
 }
 
-//
-// SPEAKER
-//
+//=====================================================================================
+//											SPEAKER
+//=====================================================================================
 
 TMRpcm tmrpcm;
 
 void initializeSpeaker()
 {
 	tmrpcm.speakerPin = 46;
-	tmrpcm.volume(1);
-	tmrpcm.quality(1);
-	// tmrpcm.play("test");
+	pinMode(46, OUTPUT);
+	tmrpcm.setVolume(50);
+	tmrpcm.play((char *)"test");
+	Serial.println(tmrpcm.isPlaying());
 }
 
-//
-// SETUP
-//
+//=====================================================================================
+//										SETUP & LOOP
+//=====================================================================================
 
 void setup()
 {
@@ -265,11 +297,8 @@ void setup()
 	initializeLCD();
 	bool hasSD = SD.begin(SD_CS_PIN);
 	initializeFadedText(hasSD);
+	initializeSpeaker();
 }
-
-//
-// LOOP
-//
 
 void loop()
 {
